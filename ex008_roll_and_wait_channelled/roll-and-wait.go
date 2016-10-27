82d3ebc3133b77c8617728d62debb7eb000db43c
David Wickes <dave.wickes@gmail.com>
2016-10-27 23:50:18 +0100

    fun

--- ex008_roll_and_wait_channelled/roll-and-wait.go
+++ ex008_roll_and_wait_channelled/roll-and-wait.go
@@ -1,25 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
-func main() {
-	scores := make([]int, 10)
-	stop := newTimer(time.Duration(time.Second * 60))
-	stopFanout := NewFanout(stop)
-
-	gameEvents := make(chan gameEvent)
-	gameEventFanin := NewFanIn(gameEvents)
-
-	for i := range scores {
-		pchan := player(strconv.Itoa(i), stopFanout.NewListener())
-		gameEventFanin.Register(pchan)
-	}
-
-	gameOver := <-stopFanout
+var players = []string{
+	"Jo",
+	"Alex",
+	"Toni",
 }
 
 type gameEvent struct {
@@ -27,9 +17,58 @@ type gameEvent struct {
 	roll   int
 }
 
-func player(name string, stop <-chan bool) (roll <-chan gameEvent) {
-	roll = make(chan gameEvent)
-	return roll
+type die <-chan int
+
+func main() {
+	scores := make(map[string]int)
+	stopFanout := NewFanout(newTimer(time.Duration(time.Second * 10)))
+	stop := stopFanout.NewListener()
+
+	gameEvents := make(chan gameEvent)
+	go gameLoop(gameEvents, scores)
+
+	gameEventFanin := NewFanIn(gameEvents)
+
+	for _, name := range players {
+		pchan := player(name, stopFanout.NewListener())
+		gameEventFanin.Register(pchan)
+	}
+
+	<-stop
+	fmt.Println(scores)
+}
+
+func gameLoop(events <-chan gameEvent, scores map[string]int) {
+	for {
+		e := <-events
+		fmt.Println(e.String())
+		scores[e.player] += e.roll
+	}
+}
+
+func (e *gameEvent) String() string {
+	return fmt.Sprintf("%s rolled %d, waiting %d seconds", e.player, e.roll, 6-e.roll)
+}
+
+func player(name string, stop <-chan bool) <-chan gameEvent {
+	events := make(chan gameEvent)
+	die := newDie()
+
+	go playerLoop(name, stop, events, die)
+	return events
+}
+
+func playerLoop(name string, stop <-chan bool, events chan<- gameEvent, d die) {
+	for {
+		select {
+		case x := <-d:
+			events <- gameEvent{name, x}
+			time.Sleep(time.Second * time.Duration(6-x))
+		case <-stop:
+			fmt.Println("%s says Game Over", name)
+			return
+		}
+	}
 }
 
 func rollDie() int {
@@ -45,4 +84,16 @@ func newTimer(d time.Duration) <-chan bool {
 	}()
 
 	return timer
+}
+
+func newDie() die {
+	die := make(chan int)
+	go roll(die)
+	return die
+}
+
+func roll(die chan<- int) {
+	for {
+		die <- rand.Intn(5) + 1
+	}
 }
